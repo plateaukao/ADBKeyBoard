9f3fc6817b9ae689fd2409060792432fad848d14
Daniel Kao <daniel.kao@gmail.com>
2018-06-09 17:29:36 +0800

    convert to kotlin implementation

---
+++ keyboardservice/src/main/java/com/android/adbkeyboard/AdbIME.kt
@@ -0,0 +1,72 @@
+package com.android.adbkeyboard
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.inputmethodservice.InputMethodService
+import android.view.KeyEvent
+import android.view.View
+
+class AdbIME : InputMethodService() {
+    private var mReceiver: BroadcastReceiver? = null
+
+    override fun onCreateInputView(): View {
+        val mInputView = layoutInflater.inflate(R.layout.view, null)
+
+        if (mReceiver == null) {
+            mReceiver = AdbReceiver()
+            val filter = IntentFilter(IME_MESSAGE).apply {
+                addAction(IME_CHARS)
+                addAction(IME_KEYCODE)
+                addAction(IME_EDITOR_CODE)
+            }
+            registerReceiver(mReceiver, filter)
+        }
+
+        return mInputView
+    }
+
+    override fun onDestroy() {
+        mReceiver?.let { unregisterReceiver(it) }
+        super.onDestroy()
+    }
+
+    internal inner class AdbReceiver : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            when (intent.action) {
+                IME_MESSAGE ->
+                    intent.getStringExtra("msg")?.let {
+                        currentInputConnection?.commitText(it, 1)
+                    }
+                IME_CHARS ->
+                    intent.getIntArrayExtra("chars")?.let {
+                        val msg = String(it, 0, it.size)
+                        currentInputConnection?.commitText(msg, 1)
+                    }
+                IME_KEYCODE -> {
+                    val code = intent.getIntExtra("code", -1)
+                    if (code != -1) {
+                        currentInputConnection?.sendKeyEvent(KeyEvent(KeyEvent.ACTION_DOWN, code))
+                    }
+                }
+
+                IME_EDITOR_CODE -> {
+                    val code = intent.getIntExtra("code", -1)
+                    if (code != -1) {
+                        currentInputConnection?.performEditorAction(code)
+                    }
+                }
+                else -> {}
+            }
+        }
+    }
+
+    companion object {
+        private const val IME_MESSAGE = "ADB_INPUT_TEXT"
+        private const val IME_CHARS = "ADB_INPUT_CHARS"
+        private const val IME_KEYCODE = "ADB_INPUT_CODE"
+        private const val IME_EDITOR_CODE = "ADB_EDITOR_CODE"
+
+    }
+}
